57586a31ad6fb434a4bb2935e70d04caca5aba75
gafarchik <50053994+gafarchik@users.noreply.github.com>
2021-02-26 17:43:20 +0200

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,16 +1,11 @@
 import cv2
-
-ds_factor=0.6
-class VideoCamera(object):
-    def __init__(self):
-       self.video = cv2.VideoCapture(0)
-    
-    def __del__(self):
-        self.video.release()
-def get_frame(self):
-        ret, frame = self.video.read()
-        frame=cv2.resize(frame,None,fx=ds_factor,fy=ds_factor,
-        interpolation=cv2.INTER_AREA)                    
-        gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-        ret, jpeg = cv2.imencode('.jpg', frame)
-        return jpeg.tobytes()
+camera = cv2.VideoCapture(0)
+def gen_frames():  
+        while True:
+            success, frame = camera.read()
+            if not success:
+                break
+            else:
+                ret, buffer = cv2.imencode('.jpg', frame)
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
