83131b27921958eeff788ff0a8e10655f76401a3
gafarchik <50053994+gafarchik@users.noreply.github.com>
2021-02-27 15:05:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, redirect, url_for, request
 from camera import gen_frames
 app = Flask(__name__)
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 @app.route('/video_feed')
