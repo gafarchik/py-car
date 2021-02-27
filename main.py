66aab2953e1a155c78a84cddfcd647d2970ec9f6
gafarchik <50053994+gafarchik@users.noreply.github.com>
2021-02-27 17:49:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template, Response, redirect, url_for, request
-from camera import gen_frames
+from camera import gen_frames, rec_video
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+@app.route('/record', methods=['GET', 'POST'])
+def record():
+	if request.method == 'POST':
+	    if request.form.get('Start')== 'Start':
+	        rec_video(1)
+	        return render_template("index.html")
+	    elif request.form.get('Stop')== 'Stop':
+	        rec_video(0)
+	        return render_template("index.html")
+	    else:
+	        return render_template("index.html")
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 if __name__ == '__main__':
-    app.run(host='192.168.0.108',port="7777", debug=True)
+    app.run(host='192.168.88.218',port="7777", debug=True)
