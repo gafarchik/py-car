6389543043ea5a11e0b6d0990d24eae39db221ec
gafarchik <50053994+gafarchik@users.noreply.github.com>
2021-02-27 17:48:44 +0200

    Update camera.py

--- camera.py
+++ camera.py
@@ -9,3 +9,19 @@ def gen_frames():
                 ret, buffer = cv2.imencode('.jpg', frame)
                 frame = buffer.tobytes()
                 yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+def rec_video(a):
+    fourcc = cv2.VideoWriter_fourcc(*'DIVX') 
+    video_writer = cv2.VideoWriter("output.avi", fourcc, 15, (680, 480))
+
+    # record video
+    while a<1:
+        ret, frame = camera.read()
+        if ret:
+            video_writer.write(frame)
+
+        else:
+            break
+
+    camera.release()
+    video_writer.release()
+    cv2.destroyAllWindows()
