0c51fd4e131709300dfbd9c2898a3f908147a041
gafarchik <50053994+gafarchik@users.noreply.github.com>
2021-03-01 19:19:05 +0200

    Update main

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from flask import Flask, render_template, Response, redirect, url_for, request
-from camera import gen_frames, rec_video, take_photo
+from camera import gen_frames, video, take_photo,stop_video
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 @app.route('/record', methods=['GET', 'POST'])
 def record():
-	if request.method == 'POST':
-	    if request.form.get('Start')== 'Start':
-	        rec_video(1)
-	        return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-	    elif request.form.get('Stop')== 'Stop':
-	        rec_video(0)
-	        return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-	    else:
-	        return render_template("index.html")
+    if request.method == 'POST':
+        if request.form.get('Start')== 'Start':
+            video()
+            return render_template('index.html')
+            return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+        elif request.form.get('Stop')== 'Stop':
+            stop_video()
+            index()
+            return render_template('index.html')
+            return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+        else:
+            return render_template("index.html")
+            return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 @app.route('/photo', methods=['GET', 'POST'])
 def photo():
 	if request.method == 'POST':
