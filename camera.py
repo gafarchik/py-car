2b03947268c1e6975323014df300b3e968306e12
gafarchik <50053994+gafarchik@users.noreply.github.com>
2021-03-03 20:35:19 +0200

    Update camera.py

--- camera.py
+++ camera.py
@@ -3,7 +3,6 @@ import numpy as np
 camera = cv2.VideoCapture(0)
 num = 0
 vnum = 0
-re = True
 def gen_frames():  
         while True:
             success, frame = camera.read()
@@ -13,27 +12,25 @@ def gen_frames():
                 ret, buffer = cv2.imencode('.jpg', frame)
                 frame = buffer.tobytes()
                 yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-def video():
-    global re
-    global vnum
-    if re == False:
-        return None
-    else:	
-	    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-	    out = cv2.VideoWriter('output'+str(vnum)+'.avi', fourcc, 15.0, (640,480))
-	    vnum+=1
-	    while True:
-	        ret, frame = camera.read()
-	        if ret==True:
-	            frame = cv2.flip(frame,180)
-	        else:
-	        	break
-	        out.write(frame)
-	    camera.release()
-	    out.release()
-def stop_video():
-    re == False
+class Recordvideo():	
+    def video():
+        global vnum
+        global out
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        out = cv2.VideoWriter('output'+str(vnum)+'.avi', fourcc, 30.0, (640,480))
+        vnum+=1
+        while True:
+            ret, frame = camera.read()
+            if ret==True:
+                frame = cv2.flip(frame,180)
+            else:
+                break
+            out.write(frame)
+        camera.release()
+        out.release()
+    def stop_video():
+        global out
+        out.release()
 def take_photo():
     global num
     num+=1
