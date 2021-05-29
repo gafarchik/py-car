764437d179eab1f248b7100f4e6ec89ebda5880c
gafarchik <50053994+gafarchik@users.noreply.github.com>
2021-05-29 08:27:05 +0300

    Delete right.py

--- right.py
+++
@@ -1,9 +0,0 @@
-import RPi.GPIO as GPIO
-
-def right():
-    GPIO.cleanup()
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(1,GPIO.OUT)
-    GPIO.setup(4,GPIO.OUT)
-    GPIO.output(1,True)
-    GPIO.output(4,True)
