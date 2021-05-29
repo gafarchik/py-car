4900aa1bd357d5c2ff9052c638a1e21c070bc076
gafarchik <50053994+gafarchik@users.noreply.github.com>
2021-05-29 08:29:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, Response, redirect, url_for, request
 from camera import gen_frames, Recordvideo , take_photo
+from control import allStop,forwardDrive,reverseDrive,TurnLeft,TurnRight,center
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -20,6 +21,35 @@ def redirect():
 @app.route('/',methods=['GET','POST'])
 def panel():
     return render_template("panel.html")
+@app.route('/move',methods=['GET','POST'])
+def move():
+    if request.method == 'POST':
+        if request.form.get('forward') == '↑':
+            forwardDrive()
+            return render_template('panel.html')
+            return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+        elif request.form.get('back' )== '↓':
+            reverseDrive()
+            return render_template('panel.html')
+            return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+        elif request.form.get('left' )== '←':
+            TurnLeft()
+            return render_template('panel.html')
+            return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+        elif request.form.get('right')== '→':
+            TurnRight()
+            return render_template('panel.html')
+            return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+        elif request.form.get('stop' )== 'stop':
+            allStop()
+            return render_template('panel.html')
+            return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+        elif request.form.get('center') == 'center':
+            center()
+            return render_template('panel.html')
+            return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+        else:
+            pass # unknown
 @app.route('/record', methods=['GET', 'POST'])
 def record():
     if request.method == 'POST':
@@ -45,4 +75,4 @@ def photo():
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 if __name__ == '__main__':
-    app.run(host='192.168.50.77',port="7777", debug=False)
+    app.run(host='0.0.0.0',port="7777", debug=False)#192.168.50.77
