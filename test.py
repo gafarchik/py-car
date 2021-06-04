dbc0ced5b195f1863f6fb4ed467d84402098f084
gafarchik <50053994+gafarchik@users.noreply.github.com>
2021-06-04 21:52:40 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,20 @@
 from control import *
-from time import sleep
-test = drive
-print("forward")
-test.movefor(100)
-sleep(1)
-print("back")
-test.moveback(100)
-sleep(1)
-print("right")
-test.moveright()
-sleep(1)
-print("left")
-test.moveleft()
-sleep(1)
-print("brake")
-test.brake()
-sleep(1)
-
-test.standby(True)
-test.standby(False)
+test = drive(26,19,5,21,6,13,4,False)
+def forward():
+    print("forward")
+    test.movefor(100)
+def back():
+    print("back")
+    test.moveback(100)
+def right():
+    print("right")
+    test.moveright()
+def left():
+    print("left")
+    test.moveleft()
+def brake():
+    print("brake")
+    test.brake()
+def center():
+	print("center")
+	test.center()
